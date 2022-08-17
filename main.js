49634c6621a83d396f907f38daa645350cb1a8bc
dusmuxamedovashirin <dusmukhamtdovashirin@gmail.com>
2022-08-18 01:47:19 +0300

    first

---
+++ main.js
@@ -0,0 +1,32 @@
+ 
+
+
+// var num = 3;
+
+// var name = Shirin;
+ 
+// var hasGrown = true;
+
+// null
+
+ 
+// var student = ["student"]
+
+// undefined
+// Symbol
+
+ 
+// var person = {
+//     name: "",
+//     age:""
+
+// }
+
+ 
+
+// let result;
+// result = '3' + 2;
+// console.log(result);
+
+
+
